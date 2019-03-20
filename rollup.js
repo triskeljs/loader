59cd948fedcfe848fc542d6c776a9bca8ebf8f2e
Jesús Germade <jesus@germade.es>
2019-03-21 00:09:39 +0100

    fixed intentation

--- rollup.js
+++ rollup.js
@@ -2,16 +2,16 @@
 const triskelLoader = require('./loader')
 
 module.exports = function json() {
-	return {
-		name: 'json',
+  return {
+    name: 'json',
 
-		transform(html, filepath) {
-			if( filepath.slice(-5) !== '.html' ) return null
+    transform(html, filepath) {
+      if( filepath.slice(-5) !== '.html' ) return null
 
-			return {
-				code: triskelLoader(html),
-				map: { mappings: '' }
-			}
-		}
-	}
+      return {
+        code: triskelLoader(html),
+        map: { mappings: '' }
+      }
+    }
+  }
 }
